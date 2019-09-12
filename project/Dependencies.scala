387c69f480c78a1bed93abfce2bb85b60958b92f
mr-git <8796159+mr-git@users.noreply.github.com>
2019-09-12 08:50:32 +0300

    use Scala 2.12.10 and 2.13.0, SBT 1.3.0, Cats 2.0.0 (#2)
    * Scala 2.12.10 and 2.13.0, SBT 1.3.0, Cats 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,20 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.7"
-  val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.1"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "0.0.12"
-  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.0.1"
-  val `safe-actor`     = "com.evolutiongaming" %% "safe-actor"     % "2.0.5"
+  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.8"
+  val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.2"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "0.0.30"
+  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.0.6"
+  val `safe-actor`     = "com.evolutiongaming" %% "safe-actor"     % "2.0.7"
 
   object Cats {
-    private val version = "1.6.0"
+    private val version = "2.0.0"
     val core   = "org.typelevel" %% "cats-core"   % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
+    val effect = "org.typelevel" %% "cats-effect" % "2.0.0"
   }
 
   object Akka {
-    private val version = "2.5.23"
+    private val version = "2.5.25"
     val actor              = "com.typesafe.akka" %% "akka-actor"            % version
     val cluster            = "com.typesafe.akka" %% "akka-cluster"          % version
     val sharding           = "com.typesafe.akka" %% "akka-cluster-sharding" % version
