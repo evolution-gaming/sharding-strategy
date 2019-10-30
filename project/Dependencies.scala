d49b6630fd1ff4ecf2d5bb077a5c532716912830
Scala Steward <me@scala-steward.org>
2019-10-30 21:18:58 +0100

    Update ddata-tools to 2.0.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalatest        = "org.scalatest"       %% "scalatest"      % "3.0.8"
   val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.2"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "1.0.4"
-  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.0.6"
+  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.0.7"
   val `safe-actor`     = "com.evolutiongaming" %% "safe-actor"     % "2.0.7"
 
   object Cats {
