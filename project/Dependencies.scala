53e497cd4d493a6fc446b0c93460ce300af94c5c
Scala Steward <me@scala-steward.org>
2021-09-17 23:34:00 +0200

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.2.9"
+  val scalatest        = "org.scalatest"       %% "scalatest"      % "3.2.10"
   val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.2"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "2.2.3"
   val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.0.8"
