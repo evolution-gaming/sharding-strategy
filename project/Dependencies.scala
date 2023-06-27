ac854e78f066b5bed17314f49dd1d63a714efc5b
Ilya Surkov <ilyas9406@yandex.ru>
2023-06-27 15:28:38 +0200

    Bump smetrics, cats-helper, akka, ddata-tools, cats, cats-effect (#163)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,19 +4,19 @@ object Dependencies {
 
   val scalatest        = "org.scalatest"       %% "scalatest"      % "3.2.9"
   val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.2"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "3.0.3"
-  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "3.0.0"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "3.6.0"
+  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "3.1.0"
   val `safe-actor`     = "com.evolutiongaming" %% "safe-actor"     % "3.0.0"
 
   object Cats {
-    private val version = "2.7.0"
-    private val effectVersion = "3.3.11"
+    private val version = "2.9.0"
+    private val effectVersion = "3.4.8"
     val core   = "org.typelevel" %% "cats-core"   % version
     val effect = "org.typelevel" %% "cats-effect" % effectVersion
   }
 
   object Akka {
-    private val version = "2.6.8"
+    private val version = "2.6.20"
     val actor              = "com.typesafe.akka" %% "akka-actor"            % version
     val cluster            = "com.typesafe.akka" %% "akka-cluster"          % version
     val sharding           = "com.typesafe.akka" %% "akka-cluster-sharding" % version
