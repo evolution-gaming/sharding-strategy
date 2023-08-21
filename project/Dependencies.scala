e4e6006c24216cbb1bdaba959860f41093140859
Artem <akhoruzhenko@evolution.com>
2023-08-21 12:12:25 +0200

    bump smetrics

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scalatest        = "org.scalatest"       %% "scalatest"      % "3.2.9"
   val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.2"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"    % "2.12.0"
-  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.1.0"
+  val `ddata-tools`    = "com.evolutiongaming" %% "ddata-tools"    % "2.2.0"
   val `safe-actor`     = "com.evolutiongaming" %% "safe-actor"     % "3.0.0"
 
   object Cats {
