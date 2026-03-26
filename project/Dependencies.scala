012942612965f063afa9a02c46117665c387d750
Scala Steward <scala_steward@virtuslab.com>
2026-03-26 18:42:53 +0000

    Update scalatest to 3.2.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies {
 
-  val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.19"
+  val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.20"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.12.2"
   val `ddata-tools` = "com.evolutiongaming" %% "ddata-tools" % "4.1.0"
 
